bea938b032ef64925001980b55a2072e27c1b7cb
Elías JR <167987056+eliasjr89@users.noreply.github.com>
2024-08-27 21:16:57 +0200

    Update navbar.js

--- src/js/component/navbar.js
+++ src/js/component/navbar.js
@@ -1,17 +1,22 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faPlus } from "@fortawesome/free-solid-svg-icons";
 
-export const Navbar = () => {
-	return (
-		<nav className="navbar navbar-light bg-light mb-3">
-			<Link to="/">
-				<span className="navbar-brand mb-0 h1">React Boilerplate</span>
-			</Link>
-			<div className="ml-auto">
-				<Link to="/demo">
-					<button className="btn btn-primary">Check the Context in action</button>
-				</Link>
-			</div>
-		</nav>
-	);
-};
+export const Navbar = () => (
+  <nav className="d-flex justify-content-evenly navbar navbar-expand-lg navbar-light bg-light mb-3">
+    <Link to="/" className="navbar-brand">
+      <button type="button" className="btn btn-light">
+        <h1>Contact List with React & Fetch</h1>
+      </button>
+    </Link>
+    <div className="ml-auto">
+      <Link to="/new-contact">
+        <button className="btn btn-success">
+          <FontAwesomeIcon icon={faPlus} className="me-2" />
+          Add New Contact
+        </button>
+      </Link>
+    </div>
+  </nav>
+);
